5efd29174e37cae8dcd1db8669af1d4391cd8552
洪镇权 <saoraozhe3hao@126.com>
2019-05-05 19:30:24 +0800

    项目初始化

---
+++ src/main/java/com/xianqingzao/yequxiaoquan/YequxiaoquanApplication.java
@@ -0,0 +1,13 @@
+package com.xianqingzao.yequxiaoquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YequxiaoquanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YequxiaoquanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xianqingzao/yequxiaoquan/YequxiaoquanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xianqingzao.yequxiaoquan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YequxiaoquanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
